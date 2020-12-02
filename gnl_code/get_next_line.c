69705475f08895df21d7ff4274b9b487452c1c8d
moonorange <monoma632@gmail.com>
2020-12-02 20:51:13 +0900

    add: test.c

--- gnl_code/get_next_line.c
+++ gnl_code/get_next_line.c
@@ -6,7 +6,27 @@
 /*   By: kkida <kkida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 08:41:03 by kkida             #+#    #+#             */
-/*   Updated: 2020/11/29 08:41:06 by kkida            ###   ########.fr       */
+/*   Updated: 2020/12/02 20:38:35 by kkida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int		get_next_line(int fd, char **line)
+{
+	char			*buffer;
+	ssize_t			nbytes;
+	static char		*text[4090];
+
+	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (fd < 0 || !line || BUFFER_SIZE <= 0 || !buffer)
+		return (ERROR);
+	while ((nbytes = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[nbytes] = '\0';
+		if (*text)
+		{
+			
+		}
+	}
+}
--- gnl_code/get_next_line.h
+++ gnl_code/get_next_line.h
@@ -6,7 +6,18 @@
 /*   By: kkida <kkida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 08:41:05 by kkida             #+#    #+#             */
-/*   Updated: 2020/11/29 08:41:07 by kkida            ###   ########.fr       */
+/*   Updated: 2020/12/02 20:26:20 by kkida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 32
+# define ERROR -1
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
