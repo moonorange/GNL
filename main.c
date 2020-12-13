474712627a2d7b7bfcb8a34ce3ae5ed319033dcf
moonorange <monoma632@gmail.com>
2020-12-13 13:05:21 +0900

    add: test tool

--- main.c
+++ main.c
@@ -1,30 +1,63 @@
 #include "gnl_code/get_next_line.h"
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-int   main(int ac, char **av)
+int	main(int ac, char **av)
 {
-  char  *line;
-  int   fd1;
-  int   fd2;
+	char	*line;
+	int		ret;
+	int		i;
+	int		*fds;
+	int		index;
+	int		k;
+	int		f;
+	i = 1;
+	if (ac <= 1)
+		return (0);
+	else if (ac >= 2)
+	{
+		index = 1;
+		fds = (int *)malloc(sizeof(int) * (ac));
+		while (index < ac)
+		{
+			fds[index - 1] = open(av[index], O_RDONLY);
+			index++;
+		}
+		index = 0;
+		printf("_________________result_________________\n");
+		while (1)
+		{
+			k = 0;
+			f = 0;
+			if(fds[index] != -1)
+			{
+				ret = get_next_line(fds[index], &line);
+				if (ret <= 0)
+				{
+					fds[index] = -1;
+					while  (k < ac - 1)
+					{
+						if (fds[k] == -1)
+							f += 1;
+						k++;
+					}
+					if (f == ac - 1)
+						break ;
+				}
+				printf("%d:%s\n", i++, line);
+				free(line);
+			}
+			if (ret == -1)
+				break;
+			index++;
+			if (index == ac - 1)
+				index = 0;
 
-  ac = 0;
-  av = 0;
-  fd1 = open("test.txt", O_RDONLY);
-  fd2 = open("test2.txt", O_RDONLY);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  printf("%d\n", get_next_line(fd2, &line));
-  printf("%s\n", line);
-  return (0);
+		}
+		free(line);
+		printf("TRUE%d:%s\n", i, line);
+		printf("_________________result_________________\n");
+	}
+	return (0);
+
 }
